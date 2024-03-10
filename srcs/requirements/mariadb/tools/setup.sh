f383095fc9742fb7e612758e79cdfb5c286481b9
ynslee <112563002+ynslee@users.noreply.github.com>
2024-03-10 17:56:05 +0200

    it worksssss

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,33 +1,41 @@
 #!/bin/sh
 
-if [ -z "$MARIADB_ROOT_PASSWORD" ]; then
-  echo "Error: MARIADB_ROOT_PASSWORD environment variable is not set."
-  exit 1
-fi
-if [ -z "$DB_USER" ] || [ -z "$MARIADB_ROOT_PASSWORD" ]; then
-  echo "Error: DB_USER and/ or DB_USER_PASSWORD environment variable is not set."
-  exit 1
-fi
+# if [ -z "$MARIADB_ROOT_PASSWORD" ]; then
+#   echo "Error: MARIADB_ROOT_PASSWORD environment variable is not set."
+#   exit 1
+# fi
+# if [ -z "$DB_USER" ] || [ -z "$MARIADB_ROOT_PASSWORD" ]; then
+#   echo "Error: DB_USER and/ or DB_USER_PASSWORD environment variable is not set."
+#   exit 1
+# fi
 
-mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql 
+mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/log/mysql
+# chown -R mysql:mysql /var/log/mysql/error.log
 touch /var/log/mysql/error.log
 
-mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm >> /dev/null
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm > /dev/null
 
 mysqld --user=mysql --bootstrap  << EOF
 USE mysql ;
 FLUSH PRIVILEGES ;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY'${MARIADB_ROOT_PASSWORD}' ;
-CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '$DB_USER_PASSWORD' ;
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' ;
-GRANT ALL PRIVILEGES ON *.* TO '${DB_USER}'@'%' ;
+DROP DATABASE IF EXISTS test ;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password' ;
+CREATE DATABASE IF NOT EXISTS inception_db CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER 'yoonslee'@'localhost' IDENTIFIED by 'password' ;
+GRANT ALL PRIVILEGES ON inception_db.* TO 'yoonslee'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'yoonslee'@'localhost' IDENTIFIED BY 'password' WITH GRANT OPTION;
+
 FLUSH PRIVILEGES ;
 
 EOF
 
-exec mysqld_safe "--defaults-file=/etc/my.cnf.d/mariadb.cnf"
+# GRANT SELECT ON mysql.* TO 'yoonslee'@'%';
+# GRANT SELECT ON inception_db.* TO 'yoonslee'@'%';
+# exec mysql_secure_installation
+exec mysqld_safe "--defaults-file=/etc/my.cnf.d/my.cnf"
+# mysqld_safe
