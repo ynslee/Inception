f10134656688899e0636b1b74134f943cc03850f
ynslee <112563002+ynslee@users.noreply.github.com>
2024-03-10 12:40:11 +0200

    --all

---
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql /error/log/mysql
+chown -R mysql:mysql /var/lib/mysql
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/log/mysql
+chown -R mysql:mysql /error/log/mysql
+touch /var/log/mysql/error.log
+
+mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql --rpm >> /dev/null
+
+mysqld --user=mysql --bootstrap  << _EOF_
+USE mysql ;
+FLUSH PRIVILEGES ;
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '{$MYSQL_ROOT_PASSWORD}' ;
+CREATE DATABASE IF NOT EXISTS {$MYSQL_HOS}T ;
+CREATE USER '{$MYSQL_USER}'@'%' IDENTIFIED BY '{$MYSQL_PASSWORD}' ;
+GRANT ALL PRIVILEGES ON {$MYSQL_HOST}.* TO '{$MYSQL_USER}'@'%' ;
+GRANT ALL PRIVILEGES ON *.* TO '{$MYSQL_USER}'@'%' ;
+FLUSH PRIVILEGES ;
+_EOF_
+
+exec mysqld_safe "--defaults-file=/etc/my.cnf.d/mariadb.cnf"
